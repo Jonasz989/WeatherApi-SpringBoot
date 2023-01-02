ef5d3edc97a5b16e6337cc867088059e1bd826b4
Jonasz989 <76077510+Jonasz989@users.noreply.github.com>
2023-01-02 14:19:53 +0100

    Initial commit

---
+++ src/main/java/com/weather/volvo/VolvoApplication.java
@@ -0,0 +1,13 @@
+package com.weather.volvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolvoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/weather/volvo/WeatherController.java
@@ -0,0 +1,2 @@
+package com.weather.volvo;public class WeatherController {
+}
